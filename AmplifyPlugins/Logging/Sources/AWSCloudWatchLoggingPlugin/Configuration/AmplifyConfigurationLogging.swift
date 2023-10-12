207cc8eaaef8f5a2b10435cb16596490979e3078
Max Stobetskyi <myeyesareblind@gmail.com>
2023-10-12 13:04:53 +0200

    NOTICKET: remove resettable altogether

--- AmplifyPlugins/Logging/Sources/AWSCloudWatchLoggingPlugin/Configuration/AmplifyConfigurationLogging.swift
+++ AmplifyPlugins/Logging/Sources/AWSCloudWatchLoggingPlugin/Configuration/AmplifyConfigurationLogging.swift
@@ -8,13 +8,6 @@
 import Foundation
 import Amplify
 
-extension AWSCognitoAuthPlugin: Resettable {
-
-    public func reset() {
-        try? makeCredentialStore().deleteCredential()
-    }
-}
-
 public struct AmplifyConfigurationLogging: Codable {
     public let awsCloudWatchLoggingPlugin: AWSCloudWatchLoggingPluginConfiguration
 }
